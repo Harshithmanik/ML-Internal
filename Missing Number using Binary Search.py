7041490f674f77aa9292db816ed890fd2162ba86
Harshithmanik <137004882+Harshithmanik@users.noreply.github.com>
2025-09-25 13:03:09 +0530

    Create Missing Number using Binary Search.py

---
+++ Missing Number using Binary Search.py
@@ -0,0 +1,13 @@
+def findMissingNumberBS(arr):
+    arr.sort() 
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if arr[mid] == mid:
+            left = mid + 1
+        else:
+            right = mid - 1
+            
+    return left
